295ba475290ddf3fa79e71acdeea8bb47e816c2c
Aníbal Vilela <anibal.vilela@vaivoa.com>
2021-11-08 12:48:15 -0300

    Ver a imagem selecionada

---
+++ 01 Storm Viewer/Storm Viewer/DetailViewController.swift
@@ -0,0 +1,34 @@
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+
+    var selectedImage: String?
+    //var totalImages = 0
+    //var imageIndex = 0
+    var imageTitle: String?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let imageTitle = imageTitle ?? selectedImage
+
+        //title = selectedImage
+        //title = "Picture \(imageIndex + 1) of \(totalim)"
+        title = imageTitle
+        navigationItem.largeTitleDisplayMode = .never
+
+        guard let selectedImage = selectedImage else { return }
+        imageView.image = UIImage(named: selectedImage)
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+}
--- 01 Storm Viewer/Storm Viewer/ViewController.swift
+++ 01 Storm Viewer/Storm Viewer/ViewController.swift
@@ -6,13 +6,20 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        title = "Storm Viewer"
+        navigationController?.navigationBar.prefersLargeTitles = true
+
         let fileManager = FileManager.default
         let path = Bundle.main.resourcePath!
         for file in try! fileManager.contentsOfDirectory(atPath: path) {
             if !file.hasPrefix("nssl") { continue }
 
             pictures.append(file)
+            pictures.append(file)
+            pictures.append(file)
         }
+
+        pictures.sort()
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -21,12 +28,23 @@ class ViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
-        // cell.textLabel?.text = pictures[indexPath.row]
+        //cell.textLabel?.text = pictures[indexPath.row] // Will be depricated
 
         var content = cell.defaultContentConfiguration()
         content.text = pictures[indexPath.row]
 
         cell.contentConfiguration = content
         return cell
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController else { return }
+
+        vc.selectedImage = pictures[indexPath.row]
+        //vc.totalImages = pictures.count
+        //vc.imageIndex = indexPath.row
+        vc.imageTitle = "Picture \(indexPath.row + 1) of \(pictures.count)"
+        
+        navigationController?.pushViewController(vc, animated: true)
     }
 }
