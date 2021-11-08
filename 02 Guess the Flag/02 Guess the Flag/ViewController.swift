69670acae05bf3710ec394eb8adc705b5c8624c2
Aníbal Vilela <anibal.vilela@vaivoa.com>
2021-11-08 15:08:29 -0300

    Selecionar a bandeira afeta o score

--- 02 Guess the Flag/02 Guess the Flag/ViewController.swift
+++ 02 Guess the Flag/02 Guess the Flag/ViewController.swift
@@ -12,16 +12,22 @@ class ViewController: UIViewController {
         "estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria",
         "poland", "russia", "spain", "uk", "us"
     ]
+    var correctAnswer = 0
     var score = 0
 
-    func askQuestion() {
+    func askQuestion(action: UIAlertAction! = nil) {
+        countries.shuffle()
+
         //button1.setImage(UIImage(named: countries[0]), for: .normal)
         //button2.setImage(UIImage(named: countries[1]), for: .normal)
         //button3.setImage(UIImage(named: countries[2]), for: .normal)
 
-        buttons.forEach({ button in
-            button.setImage(UIImage(named: countries[0]), for: .normal)
+        buttons.enumerated().forEach({ index, button in
+            button.setImage(UIImage(named: countries[index]), for: .normal)
         })
+
+        correctAnswer = Int.random(in: 0 ... 2)
+        title = countries[correctAnswer].uppercased()
     }
     
     override func viewDidLoad() {
@@ -30,10 +36,25 @@ class ViewController: UIViewController {
 
         title = "Guess the FREEDOM flag"
         buttons.forEach({ button in
+            button.layer.cornerRadius = 4
             button.layer.borderWidth = 1
             button.layer.borderColor = UIColor.lightGray.cgColor
         })
 
         askQuestion()
+    }
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        if correctAnswer == sender.tag {
+            title = "Correct"
+            score += 1
+        } else {
+            title = "Wrong"
+            score -= 1
+        }
+
+        let alertController = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        present(alertController, animated: true)
     }
 }
