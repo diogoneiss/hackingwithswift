ed8b579829326979114068892fc38f780697156d
Aníbal Vilela <anibal.vilela@vaivoa.com>
2021-11-09 09:42:27 -0300

    Concluir a primeira parte do quarto projeto

--- 04 Easy Browser/04 Easy Browser/ViewController.swift
+++ 04 Easy Browser/04 Easy Browser/ViewController.swift
@@ -1,7 +1,55 @@
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate {
+    let websites: [String: String] = [
+        "The Zig Programming Language": "https://ziglang.org",
+        "The Odin Programming Language": "https://odin-lang.org/",
+        "xkcd": "https://xkcd.com/",
+        "Linux Foundation": "https://www.linuxfoundation.org/",
+        "As The Dice Roll": "https://asthedicerollcast.com/",
+    ]
+
+    var webView: WKWebView!
+
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        let url = URL(string: "https://ziglang.org")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
+
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+    }
+
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
+    }
+
+    @objc func openTapped() {
+        let alertController = UIAlertController(title: "Open page...", message: nil, preferredStyle: .actionSheet)
+
+        websites.keys.sorted().forEach({
+            alertController.addAction(UIAlertAction(title: $0, style: .default, handler: openPage))
+        })
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+
+        alertController.popoverPresentationController?.barButtonItem = self.navigationItem.rightBarButtonItem
+
+        present(alertController, animated: true)
+    }
+
+    @objc func openPage(action: UIAlertAction) {
+        guard let requestUrl = websites[action.title!] else { return }
+        let httpsUrl = requestUrl.hasPrefix("https://") ? requestUrl : "https://" + requestUrl
+        let url = URL(string: httpsUrl)!
+
+        webView.load(URLRequest(url: url))
     }
 }
