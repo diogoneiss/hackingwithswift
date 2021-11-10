811cfd96a710e1d0593ccce64d5d2b27871c52ab
Aníbal Vilela <anibal.vilela@vaivoa.com>
2021-11-10 17:13:17 -0300

    Começando o projeto 6b

---
+++ 06b Guess the Flag/06b Guess the Flag/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  06b Guess the Flag
+//
+//  Created by ANIBAL GABRIEL MARTINS VILELA on 10/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
